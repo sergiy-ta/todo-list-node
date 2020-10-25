82fc7464f2079f332f22e864e45f9f095580b227
sergiy-ta <serhiy819@gmail.com>
2020-10-25 23:11:19 +0200

    add cluster to project

---
+++ cluster.ts
@@ -0,0 +1,21 @@
+import * as cluster from 'cluster';
+import * as os from 'os';
+
+const pid = process.pid;
+
+if (cluster.isMaster) {
+    const cpusCount = os.cpus().length;
+    console.log(`CPUs: ${cpusCount}`);
+    console.log(`Master started. Pid: ${pid}`);
+    for (let i = 0; i < cpusCount - 1; i++) {
+        const worker = cluster.fork();
+        worker.on('exit', () => {
+            console.log(`Worker died! Pid: ${worker.process.pid}`);
+            cluster.fork();
+        })
+    }
+}
+
+if (cluster.isWorker) {
+    require('./index.ts');
+}
